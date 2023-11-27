2e4e9662f7c9429f5c8dd0c56d3bd16018e0b4cc
Eliya Vahl <eliya@in3d-tech.com>
2023-11-27 17:17:15 +0200

    ts

--- src/portals/TeacherPortal.tsx
+++ src/portals/TeacherPortal.tsx
@@ -238,46 +238,46 @@ type FormData = {
   model: string;
 };
 
-const Temp = () => {
-  return (
-    <div className="sec-center">
-      <input
-        className="dropdown"
-        type="checkbox"
-        id="dropdown"
-        name="dropdown"
-      />
-      <label className="for-dropdown" htmlFor="dropdown">
-        Dropdown Menu <i className="uil uil-arrow-down"></i>
-      </label>
-      <div className="section-dropdown">
-        <a href="#">
-          Dropdown Link <i className="uil uil-arrow-right"></i>
-        </a>
-        <input
-          className="dropdown-sub"
-          type="checkbox"
-          id="dropdown-sub"
-          name="dropdown-sub"
-        />
-        <label className="for-dropdown-sub" htmlFor="dropdown-sub">
-          Dropdown Sub <i className="uil uil-plus"></i>
-        </label>
-        <div className="section-dropdown-sub">
-          <a href="#">
-            Dropdown Link <i className="uil uil-arrow-right"></i>
-          </a>
-          <a href="#">
-            Dropdown Link <i className="uil uil-arrow-right"></i>
-          </a>
-        </div>
-        <a href="#">
-          Dropdown Link <i className="uil uil-arrow-right"></i>
-        </a>
-        <a href="#">
-          Dropdown Link <i className="uil uil-arrow-right"></i>
-        </a>
-      </div>
-    </div>
-  );
-};
+// const Temp = () => {
+//   return (
+//     <div className="sec-center">
+//       <input
+//         className="dropdown"
+//         type="checkbox"
+//         id="dropdown"
+//         name="dropdown"
+//       />
+//       <label className="for-dropdown" htmlFor="dropdown">
+//         Dropdown Menu <i className="uil uil-arrow-down"></i>
+//       </label>
+//       <div className="section-dropdown">
+//         <a href="#">
+//           Dropdown Link <i className="uil uil-arrow-right"></i>
+//         </a>
+//         <input
+//           className="dropdown-sub"
+//           type="checkbox"
+//           id="dropdown-sub"
+//           name="dropdown-sub"
+//         />
+//         <label className="for-dropdown-sub" htmlFor="dropdown-sub">
+//           Dropdown Sub <i className="uil uil-plus"></i>
+//         </label>
+//         <div className="section-dropdown-sub">
+//           <a href="#">
+//             Dropdown Link <i className="uil uil-arrow-right"></i>
+//           </a>
+//           <a href="#">
+//             Dropdown Link <i className="uil uil-arrow-right"></i>
+//           </a>
+//         </div>
+//         <a href="#">
+//           Dropdown Link <i className="uil uil-arrow-right"></i>
+//         </a>
+//         <a href="#">
+//           Dropdown Link <i className="uil uil-arrow-right"></i>
+//         </a>
+//       </div>
+//     </div>
+//   );
+// };
