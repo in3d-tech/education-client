fedb4647f6c1ce1044b9eab455b3d8715d6358b3
Eliya Vahl <eliya@in3d-tech.com>
2023-12-03 14:03:48 +0200

    revert

--- src/portals/lessons/ArLesson.tsx
+++ src/portals/lessons/ArLesson.tsx
@@ -61,32 +61,60 @@ const ARComponent = () => {
   }
 
   const iframeHTML = `
-  <html>
-  <head>
-    <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
-    <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.2/aframe/build/aframe-ar.js"></script>
-  </head>
-  <body style="margin: 0px; overflow: hidden">
-    <a-scene embedded arjs>
-      <a-assets>
-        <a-asset-item id="3D-model" src="./assets/models/medical-model-new/Medical_Statue.gltf"></a-asset-item>
-      </a-assets>
-      <a-marker preset="hiro">
-        <a-entity gltf-model="#3D-model"
-        scale="0.004 0.004 0.004 position="-3 0.01 -2">
-          <a-animation
-              attribute="rotation"
-              dur="13000"
-              to="0 360 0"
-              repeat="indefinite"
-              easing="linear"
-          ></a-animation>
-        </a-entity>
-      </a-marker>
-      <a-entity camera></a-entity>
-    </a-scene>
-  </body>
+<html>
+<head>
+  <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
+  <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.2/aframe/build/aframe-ar.js"></script>
+</head>
+<body style="margin: 0px; overflow: hidden">
+  <a-scene embedded arjs>
+    <a-assets>
+      <a-asset-item id="3D-model" src="./assets/models/new-tank/tanky future new.gltf"></a-asset-item>
+    </a-assets>
+    <a-marker preset="hiro">
+      <a-entity gltf-model="#3D-model"
+      scale="0.002 0.002 0.002 position="-3 0.01 -2">
+        <a-animation
+            attribute="rotation"
+            dur="11000"
+            to="360 360 0"
+            repeat="indefinite"
+            easing="linear"
+        ></a-animation>
+      </a-entity>
+    </a-marker>
+    <a-entity camera></a-entity>
+  </a-scene>
+</body>
 </html>`;
+
+  //   const iframeHTML = `
+  //   <html>
+  //   <head>
+  //     <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
+  //     <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.2/aframe/build/aframe-ar.js"></script>
+  //   </head>
+  //   <body style="margin: 0px; overflow: hidden">
+  //     <a-scene embedded arjs>
+  //       <a-assets>
+  //         <a-asset-item id="3D-model" src="./assets/models/medical-model-new/Medical_Statue.gltf"></a-asset-item>
+  //       </a-assets>
+  //       <a-marker preset="hiro">
+  //         <a-entity gltf-model="#3D-model"
+  //         scale="0.004 0.004 0.004 position="-3 0.01 -2">
+  //           <a-animation
+  //               attribute="rotation"
+  //               dur="13000"
+  //               to="0 360 0"
+  //               repeat="indefinite"
+  //               easing="linear"
+  //           ></a-animation>
+  //         </a-entity>
+  //       </a-marker>
+  //       <a-entity camera></a-entity>
+  //     </a-scene>
+  //   </body>
+  // </html>`;
 
   return (
     <iframe
