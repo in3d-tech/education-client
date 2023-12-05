e2abf1bf2e7929ec2062462887f1454fd6c08477
Eliya Vahl <eliya@in3d-tech.com>
2023-12-05 14:36:41 +0200

    removed-jsqr

--- src/portals/lessons/ArLesson.tsx
+++ src/portals/lessons/ArLesson.tsx
@@ -1,110 +1,140 @@
-import { useEffect, useState, useRef } from "react";
-import jsQR from "jsQR";
+// import React from "react";
+
+// const frameHTML = `<a-scene embedded arjs>
+// <a-marker preset="hiro">
+// <a-box position="0 0.5 0" material="color: red;">
+//   <a-animation
+//     attribute="rotation"
+//     dur="3000"
+//     to="360 360 0"
+//     repeat="indefinite"
+//     easing="linear"
+//   ></a-animation>
+// </a-box>
+// </a-marker>
+// <a-entity camera></a-entity>
+//                   </a-scene>`;
+
+// const ARComponent = () => {
+//   return (
+//     <div key={Date.now()} dangerouslySetInnerHTML={{ __html: frameHTML }} />
+//   );
+// };
+
+// export default ARComponent;
+
+// const frameHTML = `<html><body style="margin: 0px; overflow: hidden">
+// <a-scene embedded arjs>
+//   <a-marker preset="hiro">
+//     <a-box position="0 0.5 0" material="color: red;">
+//       <a-animation
+//         attribute="rotation"
+//         dur="3000"
+//         to="360 360 0"
+//         repeat="indefinite"
+//         easing="linear"
+//       ></a-animation>
+//     </a-box>
+//   </a-marker>
+//   <a-entity camera></a-entity>
+// </a-scene>
+// </body></html>`;
+
+// const ARComponent = () => {
+//   return (
+//     <iframe
+//       srcDoc={frameHTML}
+//       style={{ width: "100vw", height: "100vh", border: "none" }}
+//     ></iframe>
+//   );
+// };
 
 type ARComponentProps = {
   isSquare?: boolean;
 };
 
 const ARComponent = ({ isSquare }: ARComponentProps) => {
-  const [qrContent, setQrContent] = useState(null);
-  const videoElem: any = useRef(null);
-
-  useEffect(() => {
-    if (navigator.mediaDevices?.getUserMedia) {
-      navigator.mediaDevices
-        .getUserMedia({ video: { facingMode: "environment" } })
-        .then((stream) => {
-          videoElem.current.srcObject = stream;
-          videoElem.current.addEventListener("play", startScanning);
-          videoElem.current.play();
-        });
-    }
-    return () => {
-      videoElem.current &&
-        videoElem.current.removeEventListener("play", startScanning);
-    };
-  }, []);
-
-  const startScanning = () => {
-    const canvas = document.createElement("canvas");
-    const context: any = canvas.getContext("2d");
-    const scan = () => {
-      context.drawImage(
-        videoElem.current,
-        0,
-        0,
-        videoElem.current.videoWidth,
-        videoElem.current.videoHeight
-      );
-      const imageData = context.getImageData(
-        0,
-        0,
-        videoElem.current.videoWidth,
-        videoElem.current.videoHeight
-      );
-
-      const qrCode: any = jsQR(
-        imageData.data,
-        imageData.width,
-        imageData.height
-      );
-      if (qrCode) {
-        setQrContent(qrCode.data);
-        videoElem.current.removeEventListener("play", scan);
-      } else {
-        requestAnimationFrame(scan);
-      }
-    };
-    scan();
-  };
-
-  if (!qrContent) {
-    return <video ref={videoElem}></video>;
+  if (!navigator.mediaDevices) {
+    return (
+      <div>
+        <p>
+          WebRTC not supported in this browser. Please use a supported browser.
+        </p>
+      </div>
+    );
   }
 
   const iframeHTML = `
-        <html>
-            <head>
-                <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
-                <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.2/aframe/build/aframe-ar.js"></script>
-            </head>
-            <body style="margin: 0px; overflow: hidden">
-                <a-scene embedded arjs>
-                ${
-                  !isSquare
-                    ? `<a-assets>
-                        <a-asset-item id="3D-model" src="./assets/models/new-tank/tanky future new.gltf"></a-asset-item>
-                    </a-assets>`
-                    : null
-                }
-                <a-marker type='pattern' url='./assets/markers/pattern-real-query-param.patt'>
-                ${
-                  isSquare
-                    ? ` <a-box position="0 0.5 0" material="color: red;">
-                <a-animation
-                    attribute="rotation"
-                    dur="3000"
-                    to="360 360 0"
-                    repeat="indefinite"
-                    easing="linear"
-                ></a-animation>
-            </a-box>`
-                    : ` <a-entity gltf-model="#3D-model"
-                    scale="0.004 0.004 0.004 position="-3 0.01 -2">
-                    <a-animation
-                        attribute="rotation"
-                        dur="11000"
-                        to="0 360 0"
-                        repeat="indefinite"
-                        easing="linear"
-                    ></a-animation>
-                  </a-entity>`
-                }  
-                    </a-marker>
-                    <a-entity camera></a-entity>
-                </a-scene>
-            </body>
-        </html>`;
+<html>
+<head>
+  <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
+  <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.2/aframe/build/aframe-ar.js"></script>
+</head>
+<body style="margin: 0px; overflow: hidden">
+  <a-scene embedded arjs>
+  ${
+    !isSquare
+      ? `<a-assets>
+          <a-asset-item id="3D-model" src="./assets/models/new-tank/tanky future new.gltf"></a-asset-item>
+        </a-assets>`
+      : null
+  }
+  <a-marker preset="hiro">
+  ${
+    isSquare
+      ? ` <a-box position="0 0.5 0" material="color: red;">
+  //       <a-animation
+  //         attribute="rotation"
+  //         dur="3000"
+  //         to="360 360 0"
+  //         repeat="indefinite"
+  //         easing="linear"
+  //       ></a-animation>
+  //     </a-box>`
+      : ` <a-entity gltf-model="#3D-model"
+            cale="0.004 0.004 0.004 position="-3 0.01 -2">
+            <a-animation
+              attribute="rotation"
+              dur="11000"
+              to="0 360 0"
+              repeat="indefinite"
+              easing="linear"
+            ></a-animation>
+          </a-entity>`
+  }  
+    </a-marker>
+    <a-entity camera></a-entity>
+  </a-scene>
+</body>
+</html>`;
+
+  //   const iframeHTML = `
+  //   <html>
+  //   <head>
+  //     <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
+  //     <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.2/aframe/build/aframe-ar.js"></script>
+  //   </head>
+  //   <body style="margin: 0px; overflow: hidden">
+  //     <a-scene embedded arjs>
+  //       <a-assets>
+  //         <a-asset-item id="3D-model" src="./assets/models/medical-model-new/Medical_Statue.gltf"></a-asset-item>
+  //       </a-assets>
+  //       <a-marker preset="hiro">
+  //         <a-entity gltf-model="#3D-model"
+  //         scale="0.004 0.004 0.004 position="-3 0.01 -2">
+  //           <a-animation
+  //               attribute="rotation"
+  //               dur="13000"
+  //               to="0 360 0"
+  //               repeat="indefinite"
+  //               easing="linear"
+  //           ></a-animation>
+  //         </a-entity>
+  //       </a-marker>
+  //       <a-entity camera></a-entity>
+  //     </a-scene>
+  //   </body>
+  // </html>`;
 
   return (
     <iframe
@@ -115,71 +145,6 @@ const ARComponent = ({ isSquare }: ARComponentProps) => {
 };
 
 export default ARComponent;
-
-// const ARComponent = ({ isSquare }: ARComponentProps) => {
-//   if (!navigator.mediaDevices) {
-//     return (
-//       <div>
-//         <p>
-//           WebRTC not supported in this browser. Please use a supported browser.
-//         </p>
-//       </div>
-//     );
-//   }
-
-//   const iframeHTML = `
-// <html>
-// <head>
-//   <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
-//   <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.2/aframe/build/aframe-ar.js"></script>
-// </head>
-// <body style="margin: 0px; overflow: hidden">
-//   <a-scene embedded arjs>
-//   ${
-//     !isSquare
-//       ? `<a-assets>
-//           <a-asset-item id="3D-model" src="./assets/models/new-tank/tanky future new.gltf"></a-asset-item>
-//         </a-assets>`
-//       : null
-//   }
-//   <a-marker type='pattern' url='./assets/markers/pattern-real-query-param.patt'>
-//   ${
-//     isSquare
-//       ? ` <a-box position="0 0.5 0" material="color: red;">
-//   //       <a-animation
-//   //         attribute="rotation"
-//   //         dur="3000"
-//   //         to="360 360 0"
-//   //         repeat="indefinite"
-//   //         easing="linear"
-//   //       ></a-animation>
-//   //     </a-box>`
-//       : ` <a-entity gltf-model="#3D-model"
-//             cale="0.004 0.004 0.004 position="-3 0.01 -2">
-//             <a-animation
-//               attribute="rotation"
-//               dur="11000"
-//               to="0 360 0"
-//               repeat="indefinite"
-//               easing="linear"
-//             ></a-animation>
-//           </a-entity>`
-//   }
-//     </a-marker>
-//     <a-entity camera></a-entity>
-//   </a-scene>
-// </body>
-// </html>`;
-
-//   return (
-//     <iframe
-//       srcDoc={iframeHTML}
-//       style={{ width: "100vw", height: "100vh", border: "none" }}
-//     ></iframe>
-//   );
-// };
-
-// export default ARComponent;
 
 // first rendition of model spinnning --->
 
@@ -205,52 +170,6 @@ export default ARComponent;
 //             easing="linear"
 //         ></a-animation>
 //       </a-entity>
-//     </a-marker>
-//     <a-entity camera></a-entity>
-//   </a-scene>
-// </body>
-// </html>`;
-
-// this is with custom code::::
-
-// const iframeHTML = `
-// <html>
-// <head>
-//   <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
-//   <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.2/aframe/build/aframe-ar.js"></script>
-// </head>
-// <body style="margin: 0px; overflow: hidden">
-//   <a-scene embedded arjs>
-//   ${
-//     !isSquare
-//       ? `<a-assets>
-//           <a-asset-item id="3D-model" src="./assets/models/new-tank/tanky future new.gltf"></a-asset-item>
-//         </a-assets>`
-//       : null
-//   }
-//   <a-marker type='pattern' url='./assets/markers/pattern-real-query-param.patt'>
-//   ${
-//     isSquare
-//       ? ` <a-box position="0 0.5 0" material="color: red;">
-//   //       <a-animation
-//   //         attribute="rotation"
-//   //         dur="3000"
-//   //         to="360 360 0"
-//   //         repeat="indefinite"
-//   //         easing="linear"
-//   //       ></a-animation>
-//   //     </a-box>`
-//       : ` <a-entity gltf-model="#3D-model"
-//             cale="0.004 0.004 0.004 position="-3 0.01 -2">
-//             <a-animation
-//               attribute="rotation"
-//               dur="11000"
-//               to="0 360 0"
-//               repeat="indefinite"
-//               easing="linear"
-//             ></a-animation>
-//           </a-entity>`
-//   }
 //     </a-marker>
 //     <a-entity camera></a-entity>
 //   </a-scene>
