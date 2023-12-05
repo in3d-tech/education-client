9a6303a57c3d6902dd78cdf7a481ab28b0c7fdbe
Eliya Vahl <eliya@in3d-tech.com>
2023-12-05 16:20:11 +0200

    ar-with-set-marker

--- src/portals/lessons/ArLesson.tsx
+++ src/portals/lessons/ArLesson.tsx
@@ -28,7 +28,7 @@ const ARComponent = ({ isSquare }: ARComponentProps) => {
         </a-assets>`
       : null
   }
-  <a-marker preset="hiro">
+  <a-marker type='pattern' url='./assets/markers/pattern-4 (2).patt'>
   ${
     isSquare
       ? ` <a-box position="0 0.5 0" material="color: red;">
@@ -91,6 +91,52 @@ export default ARComponent;
 //             easing="linear"
 //         ></a-animation>
 //       </a-entity>
+//     </a-marker>
+//     <a-entity camera></a-entity>
+//   </a-scene>
+// </body>
+// </html>`;
+
+// this one is example without custom QR ===>
+
+// const iframeHTML = `
+// <html>
+// <head>
+//   <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
+//   <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.6.2/aframe/build/aframe-ar.js"></script>
+// </head>
+// <body style="margin: 0px; overflow: hidden">
+//   <a-scene embedded arjs>
+//   ${
+//     !isSquare
+//       ? `<a-assets>
+//           <a-asset-item id="3D-model" src="./assets/models/new-tank/tanky future new.gltf"></a-asset-item>
+//         </a-assets>`
+//       : null
+//   }
+//   <a-marker preset="hiro">
+//   ${
+//     isSquare
+//       ? ` <a-box position="0 0.5 0" material="color: red;">
+//   //       <a-animation
+//   //         attribute="rotation"
+//   //         dur="3000"
+//   //         to="360 360 0"
+//   //         repeat="indefinite"
+//   //         easing="linear"
+//   //       ></a-animation>
+//   //     </a-box>`
+//       : ` <a-entity gltf-model="#3D-model"
+//             cale="0.004 0.004 0.004 position="-3 0.01 -2">
+//             <a-animation
+//               attribute="rotation"
+//               dur="11000"
+//               to="0 360 0"
+//               repeat="indefinite"
+//               easing="linear"
+//             ></a-animation>
+//           </a-entity>`
+//   }
 //     </a-marker>
 //     <a-entity camera></a-entity>
 //   </a-scene>
