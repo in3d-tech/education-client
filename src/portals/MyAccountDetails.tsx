ae4765a3f1a62a521c51b15d5d4f214113f5bcbe
Eliya Vahl <eliya@in3d-tech.com>
2023-12-17 18:21:01 +0200

    my-account

--- src/portals/MyAccountDetails.tsx
+++ src/portals/MyAccountDetails.tsx
@@ -1,7 +1,73 @@
+import { useEffect, useState } from "react";
+import { useAppContext } from "../context/appContext";
+import { Navbar } from "../navigation/Navbar";
+
+type AccountDetails = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string | number;
+} | null;
+
 export function MyAccountDetails() {
+  const [accountDetails, setAccountDetails] = useState<AccountDetails>(null);
+  const { user } = useAppContext();
+
+  console.log(user);
+  const getUserData = async () => {
+    try {
+      const response = await fetch("http://192.168.1.224:3000/account", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          userId: user?.userId,
+          method: "getAccountDetails",
+        }),
+      });
+
+      if (response.ok) {
+        const accountData = await response.json();
+        setAccountDetails(accountData);
+      } else {
+        console.error("Error fetching my lessons. Status:", response.status);
+      }
+    } catch (error) {
+      console.error("Error fetching my lessons:", error);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
   return (
     <>
-      <h1 style={{ color: "black" }}>My account details</h1>
+      <Navbar title="My Account" />
+      {accountDetails ? (
+        <div>
+          <div className="my-account-details-container">
+            <div>שם פרטי:</div>
+            <div className="my-account-deets-row">
+              {accountDetails.firstName}
+            </div>
+          </div>
+          <div className="my-account-details-container">
+            <div>שם משפחה: </div>
+            <div className="my-account-deets-row">
+              {accountDetails.lastName}
+            </div>
+          </div>
+          <div className="my-account-details-container">
+            <div>דואר אלקטרוני</div>
+            <div className="my-account-deets-row">{accountDetails.email}</div>
+          </div>
+          <div className="my-account-details-container">
+            <div>טלפון:</div>
+            <div className="my-account-deets-row">{accountDetails.phone}</div>
+          </div>
+        </div>
+      ) : (
+        <div style={{ color: "black" }}>Loading details...</div>
+      )}
     </>
   );
 }
