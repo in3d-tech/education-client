522599cca938e679dd4aa361d1a4bcb84e44c8da
Eliya Vahl <eliya@in3d-tech.com>
2024-01-11 12:51:12 +0200

    messing-with-values

--- public/threex/threex-artoolkitsource.js
+++ public/threex/threex-artoolkitsource.js
@@ -340,9 +340,9 @@ ARjs.Source.prototype.onResizeElement = function () {
     var sourceWidth = this.domElement.naturalWidth;
     var sourceHeight = this.domElement.naturalHeight;
   } else if (this.domElement.nodeName === "VIDEO") {
-    var sourceWidth = this.domElement.videoWidth;
-    var sourceHeight = this.domElement.videoHeight;
-    console.log("something abotu audio/video source", {
+    var sourceWidth = this.domElement.videoHeight;
+    var sourceHeight = this.domElement.videoWidth;
+    console.log("something abotu audio/video source (now reversed)", {
       sourceWidth,
       sourceHeight,
     });
