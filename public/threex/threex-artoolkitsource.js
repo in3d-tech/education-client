9fccf59438c8ba5da3c5c3a7118fe453971d3da5
Eliya Vahl <eliya@in3d-tech.com>
2024-01-14 16:03:11 +0200

    source-revert-context-kept

--- public/threex/threex-artoolkitsource.js
+++ public/threex/threex-artoolkitsource.js
@@ -332,8 +332,8 @@ ARjs.Source.prototype.onResizeElement = function () {
     var sourceWidth = this.domElement.naturalWidth;
     var sourceHeight = this.domElement.naturalHeight;
   } else if (this.domElement.nodeName === "VIDEO") {
-    var sourceHeight = this.domElement.videoWidth;
-    var sourceWidth = this.domElement.videoHeight;
+    var sourceWidth = this.domElement.videoWidth;
+    var sourceHeight = this.domElement.videoHeight;
     console.log({ videoWidth: sourceWidth, videoHeight: sourceHeight });
   } else {
     console.assert(false);
