25acd0904262f116bc0ff43d054624abdfd0eafb
Eliya Vahl <eliya@in3d-tech.com>
2024-05-07 15:21:28 +0300

    ts-log

--- src/portals/StudentPortal.tsx
+++ src/portals/StudentPortal.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { User } from "../App";
 import { MyLessons } from "./lessons/MyLessons";
 import { Navbar } from "../navigation/Navbar";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 // import { MyLessonsModal } from "./lessons/MyLessonsModal";
 
 type StudentPortalProps = {
