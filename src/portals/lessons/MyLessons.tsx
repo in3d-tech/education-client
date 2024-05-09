e949a16f25d918042385fde6bea633dfbc4d1bd7
Eliya Vahl <eliya@in3d-tech.com>
2024-05-09 15:06:44 +0300

    unused-third-time

--- src/portals/lessons/MyLessons.tsx
+++ src/portals/lessons/MyLessons.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAppContext } from "../../context/appContext";
 import { formatDate } from "../../common/logic/formatDate";
